a401f40187510f2ae8246c8caa31f79c69f7a7e3
Nicolas Delaby <nicolas@noa.one>
2017-01-02 11:30:54 +0100

    fixup! Add feature to express conditional join on queryset

--- tests/filtered_relation/tests.py
+++ tests/filtered_relation/tests.py
@@ -18,21 +18,25 @@ class FilteredRelationTests(TestCase):
         cls.editor_a = Editor.objects.create(name='a')
         cls.editor_b = Editor.objects.create(name='b')
 
-        cls.book1 = Book.objects.create(title='Poem by Alice',
-                                        editor=cls.editor_a,
-                                        author=cls.author1)
+        cls.book1 = Book.objects.create(
+            title='Poem by Alice',
+            editor=cls.editor_a,
+            author=cls.author1)
 
-        cls.book2 = Book.objects.create(title='The book by Jane A',
-                                        editor=cls.editor_b,
-                                        author=cls.author2)
+        cls.book2 = Book.objects.create(
+            title='The book by Jane A',
+            editor=cls.editor_b,
+            author=cls.author2)
 
-        cls.book3 = Book.objects.create(title='The book by Jane B',
-                                        editor=cls.editor_b,
-                                        author=cls.author2)
+        cls.book3 = Book.objects.create(
+            title='The book by Jane B',
+            editor=cls.editor_b,
+            author=cls.author2)
 
-        cls.book4 = Book.objects.create(title='The book by Alice',
-                                        editor=cls.editor_a,
-                                        author=cls.author1)
+        cls.book4 = Book.objects.create(
+            title='The book by Alice',
+            editor=cls.editor_a,
+            author=cls.author1)
         cls.author1.favourite_books.add(cls.book2)
         cls.author1.favourite_books.add(cls.book3)
 
